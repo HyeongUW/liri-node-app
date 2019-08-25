d355882db3ebc969e70d1bedc183f865385ad5be
Hyeong Suk Kim <whddkf2004@gmail.com>
2019-08-24 22:42:29 -0700

    Created starting files

--- keys.js
+++ keys.js
@@ -1,8 +1,9 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: a0ebed1fc7454471ba27bc6f51d76187,
+  secret: ccc5786e5f5949bfbaddcf0c83db4c07
 };
+
 
 
