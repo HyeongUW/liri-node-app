401528a8c4436fd9cf7589d2194afd8a79beb343
Hyeong Suk Kim <whddkf2004@gmail.com>
2019-08-25 01:05:19 -0700

    Concert this should be revisited later, working on the spotify-this-song

--- liri.js
+++ liri.js
@@ -20,12 +20,40 @@ for(var i = 2; i < process.argv.length; i++) {
 
 commandInput = commandInput.substring(0, commandInput.length - 1);
 commandInput = commandInput.toLowerCase();
+
+var splitedInput = commandInput.split(" ");
+var searchTerm = "";
+for(var i = 1; i < splitedInput.length; i++) {
+    searchTerm += " " + splitedInput[i];
+}
+searchTerm = searchTerm.substring(2, searchTerm.length - 1);
+
 //console.log("LowerCase: ", commandInput);
+//console.log("splited input: ", splitedInput);
+//console.log("search Term : ", searchTerm);
 
 //---------------------------------------------------
 
 // 3. Choose what function to execute using switch
-if(commandInput === "concert-this") {
+
+switch(splitedInput[0]) {
+    case "concert-this":
+        concertThis();
+        break;
+    case "spotify-this-song":
+        spotifyThisSong();
+        break;
+    case "movie-this":
+        movieThis();
+        break;
+    case "do-what-it-says":
+        doWhatItSays();
+        break;    
+    default:
+      console.log("please provide the input in the provided format");
+  }
+
+/* if(commandInput === "concert-this") {
     concertThis();
 } else if(commandInput === "spotify-this-song") {
     spotifyThisSong();
@@ -34,7 +62,7 @@ if(commandInput === "concert-this") {
 } else {
     // commandInput === "do-what-it-says"
     doWhatItSays();
-}
+} */
 //---------------------------------------------------
 
 // 4. Implementing each function
@@ -42,12 +70,74 @@ if(commandInput === "concert-this") {
     // 4.1 concert-this
 function concertThis() {
     console.log("concert-this executed");
+    // end point: "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    // searchTerm;
+    var searchURL = "https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp"
+    // console.log(searchURL);
+
+    axios
+        .get(searchURL)
+        .then(function(response) {
+            console.log("----------------------------");
+            console.log("Response: ", response);
+            console.log("----------------------------");
+            // If the axios was successful...
+            // Then log the body from the site!
+            console.log("Response Data: ", response.headers.date);
+            
+
+            console.log("Response Data: ", response.venue);
+            console.log("----------------------------");
+            
+        })
+  
+  
+        .catch(function(error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        }
+    );
+
+    
 
 }
 
     // 4.2 spotify-this-song
 function spotifyThisSong() {
     console.log("spotifyThisSong executed");
+    
+    
+    spotify
+        .search({ type: 'track', query: searchTerm })
+        .then(function(response) {
+            //console.log(response); 
+            //console.log(response.tracks.items.length);
+            
+            for(var i = 0; i < response.tracks.items.length; i++) {
+                console.log("// Item " + (i + 1) + " ------------------------------------------------------------------------------------------");
+                console.log(response.tracks.items[i]);
+                //console.log(response.tracks.items[i].artists);
+                //console.log(response.tracks.items[i].artists[external_urls].name);
+                //console.log(response.tracks.items[i].artists.external_urls);
+            }
+        })
+        .catch(function(err) {
+            console.error('Error occurred: ' + err); 
+    });
+
 
 }
     // 4.3 movie-this
@@ -58,7 +148,7 @@ function movieThis() {
     // 4.4 do-what-it-says
 function doWhatItSays() {
     console.log("doWhatItSays executed");
-    
+
 }
 
 
